9564d118528efc50f8a840a01c3d8b8d2aadf852
Shintaro Kojima <goodies@codeout.net>
2014-11-06 02:48:05 +0900

    Selectable interface list in Ipv4's and Ipv6's form

--- configdb/app/models/gigabit_ethernet.rb
+++ configdb/app/models/gigabit_ethernet.rb
@@ -1,3 +1,8 @@
 class GigabitEthernet < ActiveRecord::Base
   belongs_to :device
+
+  # FIXME: Migrate into app/decorators along with active_decorator gem
+  def name_with_device
+    "#{device.name} - #{name}"
+  end
 end
